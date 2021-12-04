5f0ff4857910bdddf34200c955bb5846726f3ad2
LDHugeman <luisdavid.vp@gmail.com>
2021-12-04 19:11:39 +0100

    Reto 4 superado

---
+++ reto4.js
@@ -0,0 +1,54 @@
+/* ____*____        1 -> 1            
+   ___***___        2 -> 3                  
+   __*****__        3 -> 5
+   _*******_        4 -> 7
+   *********        5 -> 9
+   ____#____
+   ____#____   height = 5*/
+
+/* __*__
+   _***_  
+   *****
+   __#__
+   __#__  height = 3 */
+
+function createXmasTree(height) {
+  var tree = '';
+  if (height >= 1 && height <= 100) {
+    var asterisk = '*';
+    var asteriskx2 = '**';
+    for (var i = 0; i < height; i++) {
+      tree += tree =
+        printUnderscore(height - i) +
+        asterisk +
+        printUnderscore(height - i) +
+        '\n';
+      asterisk += asteriskx2;
+    }
+    var treeTrunk =
+      printUnderscore(height) +
+      '#' +
+      printUnderscore(height) +
+      '\n' +
+      printUnderscore(height) +
+      '#' +
+      printUnderscore(height) +
+      '\n';
+  } else {
+    console.log('Altura no válida');
+  }
+  var treeComplete = tree + treeTrunk;
+  return treeComplete;
+}
+
+console.log(createXmasTree(3));
+
+function printUnderscore(height) {
+  var underscore = '';
+  for (var i = 0; i < height - 1; i++) {
+    underscore += '_';
+  }
+  return underscore;
+}
+
+//console.log(printUnderscore(2));
