4d02c97a5ba41f5cad240855866ce294f2d6cd95
LDHugeman <luisdavid.vp@gmail.com>
2021-12-08 15:27:16 +0100

    Reto 8 superado

--- reto8.js
+++ reto8.js
@@ -7,21 +7,24 @@ const pricesAda = [3, 3, 3, 3, 3];
 export default function maxProfit(prices) {
   const pricesLength = prices.length;
   let resultado = 0;
+  let profit;
   for (let i = 0; i < pricesLength; i++) {
     let firstNumber = prices.shift();
     for (let j = 0; j < prices.length; j++) {
       let secondNumber = prices[j];
-      let profit = Math.abs(firstNumber - secondNumber);
-      if (profit > resultado) {
-        resultado = profit;
+      if (firstNumber < secondNumber) {
+        profit = Math.abs(firstNumber - secondNumber);
+        if (profit > resultado) {
+          resultado = profit;
+        }
       }
     }
   }
   return resultado || -1;
 }
 
-maxProfit(pricesBtc); // -> 16 (compra a 18, vende a 34)
-maxProfit(pricesEth); // -> 60 (compra a 10, vende a 70)
+console.log(maxProfit(pricesBtc)); // -> 16 (compra a 18, vende a 34)
+console.log(maxProfit(pricesEth)); // -> 60 (compra a 10, vende a 70)
 
-maxProfit(pricesDoge); // -> -1 (no hay ganancia posible)
-maxProfit(pricesAda); // -> -1 (no hay ganancia posible)
+console.log(maxProfit(pricesDoge)); // -> -1 (no hay ganancia posible)
+console.log(maxProfit(pricesAda)); // -> -1 (no hay ganancia posible)
